5d491d5dfaf32182005fc54b8dd7d41c32762b7c
zebbo <lebastiansotter@gmail.com>
2016-06-26 22:43:49 +0200

    Extended Main functionality with usage print

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,23 @@ import StackMachine.SMAssembler
 import System.Environment
 
 main = do
-    [fn] <- getArgs
-    (codelen,bp,initmem) <- assembleAndLoadFile fn
+    args <- getArgs
+    case args of
+        ["--help"] -> usage
+        ("run":file:[]) -> run file
+        _ -> usage
+
+run :: String -> IO ()
+run file = do
+    (codelen,bp,initmem) <- assembleAndLoadFile file
     emulate (initState 0 bp codelen initmem) getLine putStr False
+    return ()
+
+usage :: IO()
+usage = do
+    self <- getProgName
+    putStr . unlines $ 
+        concat ["Usage: ", self, " [OPTIONS]"] :
+        "--help                 Print this message" :
+        "run <file>             Assemble, load and execute code in <file>" :
+        []
